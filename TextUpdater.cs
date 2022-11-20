ceaca5b9078f51334a9321c4dc536109a1491647
AtRealDonaldDuck <69385515+AtRealDonaldDuck@users.noreply.github.com>
2022-11-20 11:14:43 +0100

    Added all started files

---
+++ TextUpdater.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Twitch_AutoTextTimer
+{
+    internal class TextUpdater
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter Timer: ");
+            TimeSpan totalMinutes = TimeSpan.FromMinutes(Double.Parse(Console.ReadLine()));
+            Console.WriteLine("Enter File Update Interval: ");
+            TimeSpan updateInterval = TimeSpan.FromMinutes(Double.Parse(Console.ReadLine()));
+
+            Console.CancelKeyPress += Reset;
+
+            for (double i = totalMinutes.Minutes; i > 0; i-=updateInterval.Minutes)
+            {
+                ChangeTxtFileValue(@"C:\Users\A\Downloads\Stream Assets\New Text Document.txt", $"Be back in {i} {(i>1?"minutes":"minute")}");
+                Wait(updateInterval);
+            }
+
+            ChangeTxtFileValue(@"C:\Users\A\Downloads\Stream Assets\New Text Document.txt", $"Be back soon");
+        }
+
+        static void Wait(TimeSpan minutes)
+        {
+            DateTime startTime = DateTime.UtcNow;
+
+            while (DateTime.UtcNow - startTime < minutes) ;//wait for timespan to end
+        }
+
+        static void ChangeTxtFileValue(string path, string value)
+        {
+            File.WriteAllText(path, value);
+        }
+
+        static void Reset(object? sender, EventArgs e)
+        {
+            ChangeTxtFileValue(@"C:\Users\A\Downloads\Stream Assets\New Text Document.txt", "");
+        }
+
+        
+    }
+}
